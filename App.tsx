381abc35d5cee948ef961541dbaf0e64b053ed00
fercubos <fernandoyiuuu@gmail.com>
2024-10-30 08:27:01 -0600

    Dependencias

--- App.tsx
+++ App.tsx
@@ -1,14 +1,29 @@
+// requerimientos de la aplicación
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDmIgzgxUO5ZMcZEOic7bsNQgoqKIvxXY4",
+  authDomain: "foodbankds.firebaseapp.com",
+  projectId: "foodbankds",
+  storageBucket: "foodbankds.appspot.com",
+  messagingSenderId: "729077541683",
+  appId: "1:729077541683:web:be1be0635cb043dae8ec46"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
